f717f22d6dbfa3a4461c4241e5704da63b31e675
Siphamandla <spsiphamandla363@gmail.com>
2014-08-06 22:19:59 +0200

    Fisrt Project

---
+++ GitTut/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitTut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi and welcome to Git:");
+            Console.WriteLine();
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Enter your Surname:");
+            string surname = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Thank you {0} {1} and enjoy using this CVS",name,surname);
+            Console.ReadLine();
+        }
+    }
+}
